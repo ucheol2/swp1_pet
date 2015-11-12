e3e15247d2e092e49533b033663d2bfdb8eb4eaf
JungYuCheol <cokr3430@gmail.com>
2015-11-12 05:44:24 +0000

    fix devise(including name column)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108095658) do
+ActiveRecord::Schema.define(version: 20151112053806) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +28,5 @@ ActiveRecord::Schema.define(version: 20151108095658) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
