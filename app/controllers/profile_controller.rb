f1434a5b5223befc87f662b9d5e7145fbdcc1196
kibakin <kibakin@snu.ac.kr>
2015-11-19 06:06:09 +0000

    profileeeeee

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,14 +3,20 @@ class ProfileController < ApplicationController
     before_action :authenticate_user!
     
     def index #index 페이지
+        unless current_user.pets.empty?
+        redirect_to '/profile/pet_list'
+        end
     end
     
     def create #create 페이지
     end
     
+    def pet_list #pet_list 페이지
+    end
+    
     def create_pet
         Pet.create(user: current_user, name: params[:name], gender: params[:gender], age: params[:age], about: params[:about], img: params[:img])
-        redirect_to :back
+        redirect_to '/profile/pet_list'
     end
     
 end
