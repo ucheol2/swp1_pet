6aab7bbbba55a47349082adfaac8176c9db60cfc
yerin <skant121@nate.com>
2015-11-19 04:21:24 +0000

    yerin 1119

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112062845) do
+ActiveRecord::Schema.define(version: 20151119041430) do
 
   create_table "pets", force: :cascade do |t|
-    t.integer  "user_id"
     t.string   "name"
     t.integer  "age"
     t.string   "gender"
     t.string   "img"
     t.text     "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
